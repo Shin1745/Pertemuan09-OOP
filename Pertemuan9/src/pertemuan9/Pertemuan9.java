0adaf33b6af1d77a526784c45b47f99ade0a3b0a
Shin1745 <127808802+Shin1745@users.noreply.github.com>
2024-05-13 12:18:55 +0800

    Add files via upload

---
+++ Pertemuan9/src/pertemuan9/Pertemuan9.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author Muhammad Agriyansyah (2201010410)
+ * Tgl: 13-05-2024
+ */
+public class Pertemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // new frm01().setVisible(true);
+        frm01 f1 = new frm01();
+        f1.setResizable(false);
+        f1.setAlwaysOnTop(true);
+        f1.setVisible(true);
+    }
+    
+}
